e8c3c399e12c42d1490254ca775d937b2a1f12ac
vishal-muralidharan <vishmuralee1006@gmail.com>
2026-09-06 22:15:20 +0530

    feat: configure global styles and fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './assets/css/style.css'
+import './assets/css/themes.css'
+import './assets/css/custom-dialogs.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
